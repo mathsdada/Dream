0ebe7fa3c6b9f5688e290f425022dd473bc6f5c0
Gangadhar Vukkesala <gangadhar.nitw@gmail.com>
2018-10-12 15:45:44 +0530

    Spinner to select stats type in Match Activity

--- app/src/main/java/com/mission2019/dreamcricket/dreamcricket/Activity/MatchActivity.java
+++ app/src/main/java/com/mission2019/dreamcricket/dreamcricket/Activity/MatchActivity.java
@@ -12,6 +12,11 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -31,6 +36,7 @@ public class MatchActivity extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private CustomViewPager mViewPager;
     private ScheduleMatch mMatch;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,24 @@ public class MatchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mSpinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(MatchActivity.this,
+                R.layout.stats_spinner_item,
+                getResources().getStringArray(R.array.stats_types));
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(spinnerAdapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MatchActivity.this, mSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         changeActionBarTitle();
     }
 
@@ -117,26 +141,5 @@ public class MatchActivity extends AppCompatActivity {
             }
             return "";
         }
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_match, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
